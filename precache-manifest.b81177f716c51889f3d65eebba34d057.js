b4d1ea8b022177b1265e9fc4c6ac8287cc384496
starguys <kjj9500@naver.com>
2020-06-09 08:08:41 +0900

    Updates

---
+++ precache-manifest.b81177f716c51889f3d65eebba34d057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2e4f75667010d6abcd75a59972f927",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "233afe8e6c8fa5ece616",
+    "url": "/movie_app_2019/static/css/main.e2a80140.chunk.css"
+  },
+  {
+    "revision": "7676ec8dfa3a757764b3",
+    "url": "/movie_app_2019/static/js/2.cba50508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.cba50508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233afe8e6c8fa5ece616",
+    "url": "/movie_app_2019/static/js/main.f7b587a2.chunk.js"
+  },
+  {
+    "revision": "f5b00c7a31981ed629ee",
+    "url": "/movie_app_2019/static/js/runtime-main.140b3552.js"
+  }
+]);
