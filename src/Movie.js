11b685a242dbccbe3821e1ccd69fd65fa8552594
starguys <kjj9500@naver.com>
2020-06-09 08:12:30 +0900

    Deploying to Github Pages
    nomadcoder

--- src/Movie.js
+++ src/Movie.js
@@ -16,7 +16,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
